7a90117619753d6af7de73932ac5785f929159b4
Ryan Taylor <59121627+ryant74@users.noreply.github.com>
2020-12-08 16:09:18 -0500

    Created model train/validate script

---
+++ modelnet_v2.py
@@ -0,0 +1,41 @@
+from __future__ import print_function, division
+import warnings
+warnings.filterwarnings("ignore")
+import os.path
+import pandas as pd
+import torch
+import torch.nn as nn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import dlib
+import os
+import argparse
+
+import sys
+sys.path.append('/usr/local/lib/python3.6/dist-packages')
+
+def rect_to_bb(rect):
+	# take a bounding predicted by dlib and convert it
+	# to the format (x, y, w, h) as we would normally do
+	# with OpenCV
+	x = rect.left()
+	y = rect.top()
+	w = rect.right() - x
+	h = rect.bottom() - y
+	# return a tuple of (x, y, w, h)
+	return (x, y, w, h)
+
+def train_validate_model():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    summary(models.resnet101(False).cuda())
+
+    
+
+if __name__ == "__main__":
+    #Please create a csv with one column 'img_path', contains the full paths of all images to be analyzed.
+    #Also please change working directory to this file.
+    dlib.DLIB_USE_CUDA = True
+    print("using CUDA?: %s" % dlib.DLIB_USE_CUDA)
+    #Run training & validation
+    train_validate_model()
