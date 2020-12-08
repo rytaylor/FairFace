af69719945e694a8a06aa1a14b6553467c07e7d0
Ryan Taylor <59121627+ryant74@users.noreply.github.com>
2020-12-08 16:27:49 -0500

    modelnet changes to test data imports

--- modelnet_v2.py
+++ modelnet_v2.py
@@ -1,17 +1,18 @@
 from __future__ import print_function, division
-import warnings
-warnings.filterwarnings("ignore")
-import os.path
-import pandas as pd
+
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
-from torchsummary import summary
-import dlib
+import matplotlib.pyplot as plt
+import time
 import os
-import argparse
+import copy
+import dlib
+import pandas as pd
 
 import sys
 sys.path.append('/usr/local/lib/python3.6/dist-packages')
@@ -26,12 +27,76 @@ def rect_to_bb(rect):
 	h = rect.bottom() - y
 	# return a tuple of (x, y, w, h)
 	return (x, y, w, h)
+'''
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
 
-def train_validate_model():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    summary(models.resnet101(False).cuda(),(3, 256, 256))
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
 
-    
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+ '''   
 
 if __name__ == "__main__":
     #Please create a csv with one column 'img_path', contains the full paths of all images to be analyzed.
@@ -39,4 +104,34 @@ if __name__ == "__main__":
     dlib.DLIB_USE_CUDA = True
     print("using CUDA?: %s" % dlib.DLIB_USE_CUDA)
     #Run training & validation
-    train_validate_model()
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    train_paths = pd.read_csv('fairface_label_train')
+    val_paths = pd.read_csv('fairafce_label_val')
+
+    print(train_paths[0])
+    '''
+
+    image_datasets = {x: datasets.ImageFolder(os.path.join(dataloc, x),
+                                            data_transforms[x])
+                    for x in ['train', 'val']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                                shuffle=True, num_workers=4)
+                for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    class_names = image_datasets['train'].classes
+    '''
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
